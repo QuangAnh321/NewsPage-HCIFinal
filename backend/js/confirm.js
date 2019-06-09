6d15e1dc4dfcd6224701a76cce093028bdfa61e8
manhhai <39932592+manhhai98@users.noreply.github.com>
2019-06-09 12:55:39 +0700

    update

--- backend/js/confirm.js
+++ backend/js/confirm.js
@@ -1,14 +1,10 @@
 function confirmDelete() {
-	var text;
-	var c = confirm('Are you sure you want to Delete this item?');
-	if (c == true) {
-    	text = "Delete Complete!";
-    	document.getElementById("confirm").innerHTML = text;
+	
+		document.getElementById("confirm").innerHTML = "Delete Success";
   		document.getElementById("confirm").style.backgroundColor = "#d3f794"
   		document.getElementById("confirm").style.color = "#4CAF50"
   		document.getElementById("confirm").style.border = "1px solid #4CAF50"
-  	}
-
+		
 }
 
 
--- backend/js/include.js
+++ backend/js/include.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
     $('#header').load('../admin/common/header.html');
     $('#footer').load('../admin/common/footer.html');
+     $('#header1').load('../editor/common/header.html');
+    $('#footer1').load('../editor/common/footer.html');
 });
